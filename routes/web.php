90f3b0326050833a7db2749608734718d31197d9
Armen Kspoyan <armenqspoyan@gmail.com>
2024-10-24 12:29:40 +0400

    new migration for blog post

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\BlogPostsController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -12,9 +13,18 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+
+    //TODO Profile Controller section
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+
+    //TODO Blog Posts Controller section
+
+    Route::controller(BlogPostsController::class)->group(function () {
+        Route::get('/blog-posts', 'index')->name('blog-posts');
+    });
 });
 
 require __DIR__.'/auth.php';
